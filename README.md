40d8a8842e3923a11e1832f2d8f56c8294a6a559
seonghun <cchoseonghun@gmail.com>
2022-10-10 13:44:30 +0900

    default project commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Node.js_Vue_starter
---
+++ frontend/src/router.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from './components/Home'
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+export default router; 
